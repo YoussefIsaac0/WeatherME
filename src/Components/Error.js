3ea588098d29f8995a6561566d48b74584fbbef8
Youssef Isaac <youssefisaac002@gmail.com>
2024-09-04 18:44:42 +0300

    making global class for messages in css

--- src/Components/Error.js
+++ src/Components/Error.js
@@ -4,8 +4,8 @@ export default function Error({errorMsg}) {
   return (
     <>
         <div style={{display:'flex', justifyContent:"center", alignItems:"center", minHeight:'99vh', flexDirection:"column", gap:"20px"}}>
-        <h2 style={{color:'white'}}>The Service isn't available right Now</h2>
-        <h3 style={{color:'white', textAlign:'center'}}>{errorMsg}</h3>
+        <h2 className='message'>The Service isn't available right Now</h2>
+        <h3 className='message'>{errorMsg}</h3>
         </div>
     </>
   )
--- src/Components/Loading.js
+++ src/Components/Loading.js
@@ -6,8 +6,8 @@ export default function Loading() {
     
     <div style={{display:'flex', justifyContent:"center", alignItems:"center", minHeight:'99vh', flexDirection:"column", gap:"20px"}}>
     <div id='loading'/>
-    <h2 style={{color:'white'}}>Loading...</h2>
-    <h3 style={{color:'white', textAlign:'center'}}>Fetching your Country Data. Make sure you gave us the permission to access your location</h3>
+    <h2 className='message'>Loading...</h2>
+    <h3 className='message'>Fetching your Country Data. Make sure you gave us the permission to access your location</h3>
     </div>
     </div>
   )
